af799716db3939a9db358b9e47f1c1ff34c88975
WebApp07 <aminezoguig@proton.me>
2023-11-08 06:39:04 -0800

    setup database

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,12 +1,50 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
+import { connectToDB } from "@/mongoose";
 import { scrapeAmazonProduct } from "../scraper/index";
+import Product from "../models/product.model";
+import {
+  getAveragePrice,
+  getHighestPrice,
+  getLowestPrice,
+} from "../scraper/utils";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
 
   try {
+    connectToDB();
+
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
+
+    if (!scrapedProduct) return;
+    let product = scrapedProduct;
+
+    const existingProduct = await Product.findOne({ url: scrapedProduct.url });
+
+    if (existingProduct) {
+      const updatePriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice },
+      ];
+
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatePriceHistory,
+        lowestPrice: getLowestPrice(updatePriceHistory),
+        highestPrice: getHighestPrice(updatePriceHistory),
+        averagePrice: getAveragePrice(updatePriceHistory),
+      };
+    }
+    const newProduct = await Product.findOneAndUpdate(
+      { url: scrapedProduct.url },
+      product,
+      { upsert: true, new: true }
+    );
+
+    revalidatePath(`/products/${newProduct._id}`);
   } catch (error: any) {
     throw new Error(`Failed to create/update product: ${error.message}`);
   }
--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -80,8 +80,6 @@ export async function scrapeAmazonProduct(url: string) {
       averagePrice: Number(currentPrice) || Number(originalPrice),
     };
 
-    console.log(data);
-
     return data;
   } catch (error: any) {
     console.log(error);
